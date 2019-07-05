f0dc67871ab1931372aa739476099e42d8508a8c
BOUHADDIOUI <manar.bouhaddioui@capgemini.com>
2019-07-05 16:52:06 +0100

    8. Component & Template Clean Up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,84 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  users : User[];
-  bool : boolean = true;
-  loaded : boolean = false;
-  showExtended : boolean = true;
-  enableAdd : boolean = true;
-  isActive : boolean = true;
-  currentClasses ={}; 
-  currentStyle ={}; 
+  users: User[];
+  loaded: boolean = false;
+  showExtended: boolean = true;
+  enableAdd: boolean = true;
+ 
 
   constructor() { }
 
   ngOnInit() {
     this.users = [
       {
-        firstName : 'david',
-        lastName : 'david',
-        age : 33,
-        address : {
-          street : 'dd',
-          city : 'd',
-          state : 'd'
+        firstName: 'david',
+        lastName: 'david',
+        age: 33,
+        address: {
+          street: 'dd',
+          city: 'd',
+          state: 'd'
         },
-        image : "https://lorempixel.com/600/600/people/3",
-        isActive : true,
-        balance : 100,
-        registered : new Date('01/02/2018 08:30:00')
+
+        isActive: true,
+
+        registered: new Date('01/02/2018 08:30:00')
       },
       {
-        firstName : 'manar',
-        lastName : 'manar',
-        age : 24,
-        address : {
-            street : 'bd',
-            city : 'test',
-            state : 'casa'
+        firstName: 'manar',
+        lastName: 'manar',
+        age: 24,
+        address: {
+          street: 'bd',
+          city: 'test',
+          state: 'casa'
         },
-        image : "https://lorempixel.com/600/600/people/2",
-        balance : 200,
-        registered : new Date('07/02/2018 08:30:00')
+
+        registered: new Date('07/02/2018 08:30:00')
       },
       {
-        firstName : 'david',
-        lastName : 'david',
-        age : 33,
-        address : {
-          street : 'dd',
-          city : 'd',
-          state : 'd'
+        firstName: 'david',
+        lastName: 'david',
+        age: 33,
+        address: {
+          street: 'dd',
+          city: 'd',
+          state: 'd'
         },
-        image : "https://lorempixel.com/600/600/people/5",
-        isActive : true,
-        balance : 100,
-        registered : new Date('01/05/2018 10:30:00')
+
+        isActive: true,
+
+        registered: new Date('01/05/2018 10:30:00')
       }];
-      this.loaded = true;
-      // this.showExtended = false;
-      this.setCurrentClasses();
-      this.setCurrentStyle();
-     }
-
-    addUser(user){
-      this.users.push(user);
-     }   
-
-    setCurrentClasses(){
-    this.currentClasses = {
-      'btn-success' : this.enableAdd,
-      'big-text'    : this.showExtended
-    }
+    this.loaded = true;
+    
+    
   }
-  setCurrentStyle(){
-    this.currentStyle = {
-      'padding-top' : this.showExtended ? '0' : '40px',
-      'font-size'   : this.showExtended ? ''  : '40px'
-    }
+
+  addUser(user) {
+    this.users.push(user);
   }
 
   
-
-
 }
